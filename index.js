aa633b2f35a442dda34008efa8eb5321f6fa8d6b
Mikhail Serebryakov <soraenjoyer@mail.ru>
2022-12-26 14:35:16 +0500

    ``/api/langCulture/game/useList``

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 
 const express = require('express')
+
+const languageCultureList = require('./data/language-culture-list.json')
+
 const app = express()
 
 app.set('environment', process.env.NODE_ENV || 'development')
@@ -9,6 +12,14 @@ app.set('port', process.env.PORT || '5000')
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.send('hello world :3'))
+
+app.post('/api/langCulture/game/useList', (req, res) => {
+  res.json({
+    isSuccess: true,
+    msg: 'request success',
+    data: languageCultureList
+  })
+})
 
 app.listen(app.get('port'), () => {
   const port = app.get('port')
